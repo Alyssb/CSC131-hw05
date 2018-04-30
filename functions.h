150d3ffc0010f9dd62b7c5e1093aa44bb3b07fa0
alyssa slayton <ajs41@missouristate.edu>
2018-04-30 15:47:03 -0500

    enums created

--- functions.h
+++ functions.h
@@ -4,8 +4,7 @@
  *
  * @brief Header file containing the implementation of functions used in Homework 5.
  * @file functions.h
- * @authors Jim Daehn <jdaehn@missouristate.edu>
- *          TODO: Replace this line with your name using the above format.
+ * @authors Alyssa Slayton <ajs41@missouristate.edu>
  */
 
 #ifndef HW05_FUNCTIONS_H
@@ -18,8 +17,10 @@
 
 #define PRESENT_VALUE_DEFINED false
 /**
- * TODO: Implement the presentValue function below the following Doxygen comments using the README and the Doxygen
- * comments as your prescription. Once you've implemented this function as prescribed, change the PRESENT_VALUE_DEFINED
+ * TODO: Implement the presentValue function below the following Doxygen
+ * comments using the README and the Doxygen
+ * comments as your prescription. Once you've implemented this function
+ * as prescribed, change the PRESENT_VALUE_DEFINED
  * macro to true and erase this entire multi-line comment.
  */
 
@@ -30,13 +31,16 @@
  * @param F The future value that you want in the account.
  * @param r The annual interest rate (expressed in decimal form, such as 0.042).
  * @param n The number of years that you plan to let the money sit in the account.
- * @return The present value, or the amount that you need to deposit today, is returned.
+ * @return The present value, or the amount that you need to deposit today,
+ * is returned.
  */
 
 #define FUTURE_VALUE_DEFINED false
 /**
- * TODO: Implement the futureValue function below the following Doxygen comments using the README and the Doxygen
- * comments as your prescription. Once you've implemented this function as prescribed, change the FUTURE_VALUE_DEFINED
+ * TODO: Implement the futureValue function below the following Doxygen comments
+ * using the README and the Doxygen
+ * comments as your prescription. Once you've implemented this function as
+ * prescribed, change the FUTURE_VALUE_DEFINED
  * macro to true and erase this entire multi-line comment.
  */
 
@@ -46,7 +50,8 @@
  * @param P The present value of the account.
  * @param i The monthly interest rate.
  * @param t The number of months.
- * @return The future value of the account after the specified time period, is returned.
+ * @return The future value of the account after the specified time period, is
+ * returned.
  */
 
 
--- main.cpp
+++ main.cpp
@@ -12,8 +12,14 @@
 #include "functions.h"
 
 /**
- * TODO: Create function prototypes for any functions you'll implement below main that you'll call from main.
+ * TODO: Create function prototypes for any functions you'll implement below
+ * main that you'll call from main.
  */
+enum class MainMenuItem{PRESENT_OPTION = 1, FUTURE_OPTION = 2, QUIT = 9};
+enum class PresentMenuItem{FUTURE_VALUE = 1, INTEREST_RATE = 2, NUMBER_YEARS = 3,
+    CALCULATE_VALUE = 4, RETURN = 5, QUIT = 9};
+enum class FutureMenuItem{PRESENT_VALUE = 1, INTEREST_RATE = 2, NUMBER_YEARS = 3,
+    CALCULATE_VALUE = 4, RETURN = 5, QUIT = 9};
 
 /**
  * The entry point for this application.
@@ -21,6 +27,7 @@
  */
 int main()
 {
+
     /**
      * TODO: Implement a menu driven program.
      *
@@ -29,31 +36,37 @@ int main()
      * 2. Select this option to experiment with the future value function.
      * 3. Select this option to end this program.
      *
-     * If the user selects option 1 of the high-level menu, then present a secondary menu that has five menu items:
+     * If the user selects option 1 of the high-level menu, then present
+     * a secondary menu that has five menu items:
      * 1. Select this option to enter the future value
      * 2. Select this option to enter the annual interest rate
      * 3. Select this option to enter the number of years
      * 4. Select this option to calculate and display the present value.
      * 5. Select this option to go back to the high-level menu
      *
-     * If the user selects option 2 of the high-level menu, then present a secondary menu that has five menu items:
+     * If the user selects option 2 of the high-level menu, then present a
+     * secondary menu that has five menu items:
      * 1. Select this option to enter the present value
      * 2. Select this option to enter the monthly interest rate
      * 3. Select this option to enter the number of months
      * 4. Select this option to calculate and display the future value.
      * 5. Select this option to go back to the high-level menu
      *
-     * If the user select option 3 of the high-level menu, the ask the user if they are sure they want to quit.
+     * If the user select option 3 of the high-level menu, the ask the user if
+     * they are sure they want to quit.
      * 1. If the user does not want to quit, the high-level menu is displayed
      * 2. If the user really does want to quit, the program ends.
      *
-     * Strive for modularity, i.e., do not have a bloated main function; points will be deducted if you don't make use
+     * Strive for modularity, i.e., do not have a bloated main function; points
+     * will be deducted if you don't make use
      * of a modular approach.
      */
     return EXIT_SUCCESS;
 }
 
 /**
- * TODO: Implement any helper functions specified in your forward declarations that precede the main function here.
- * Erase this multi-line comment when you have finished implementing your helper functions.
+ * TODO: Implement any helper functions specified in your forward declarations
+ * that precede the main function here.
+ * Erase this multi-line comment when you have finished implementing your helper
+ * functions.
  */
